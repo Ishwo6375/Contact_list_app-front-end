aa69f99bc07ddef27983392f44cfe7f5d0fcfbad
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-06-09 18:49:32 -0400

    added input field to form

--- src/components/EmployeeForm/AddEmployeeForm.js
+++ src/components/EmployeeForm/AddEmployeeForm.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+
 
 function AddEmployeeForm() {
-    return (
-        <div>
-            <h1>add employee</h1>
-        </div>
-    )
+  return (
+    <div className="div-container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="add-hire">Add New Hire</h2>
+
+        <form>
+          <div>
+            <input className="my-2" type="text" placeholder="Enter Name" name="name" />
+          </div>
+
+           <div>
+            <input className="my-2" type="text" placeholder="Enter UserName" name="username" />
+          </div>
+
+           <div>
+            <input className="my-2" type="email" placeholder="Enter E-mail Address" name="email" />
+          </div>
+
+          <div>
+            <input className="my-2" type="text" placeholder="Enter Phone Number" name="phone" />
+          </div>
+
+          <button className="my-2 btn-primary">Add Employee</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default AddEmployeeForm
+export default AddEmployeeForm;
