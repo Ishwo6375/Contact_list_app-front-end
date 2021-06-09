397ebe3dd228a5678f67eeb72fe3bd55b79feb31
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-06-09 13:40:09 -0400

    created contact.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Home from "./components/pages/Home";
+import About from "./components/About";
+import Contact from "./components/pages/Contact";
 function App() {
 
 return(
 <div>
   <Home />
+  <About />
+  <Contact />
 </div>
 
 )
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Contact() {
+    return (
+        <div className="py-4">
+            <h1>Contact Page</h1>
+        </div>
+    )
+}
+
+export default Contact
